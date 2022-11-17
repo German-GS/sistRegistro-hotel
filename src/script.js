f4450e9965c1d6dc1583dea3fcf639cf0320bd4c
German García S <german.gs19@gmail.com>
2022-11-16 20:33:12 -0600

    Save data in array

--- src/script.js
+++ src/script.js
@@ -1,50 +1,13 @@
 var getData = function(){
     persona=[]
+    for (var i = 0; i <=4; i++){
+        persona[i]=document.getElementById("persona"+(i+1)).value
+    }
+
     persona.forEach(element => {
-        persona=element
+        console.log(element)
         
     });
-    
-    const name= document.getElementById("name").value 
-    const last_name=document.getElementById("last_name").value 
-    const number_nights=document.getElementById("num_Nights").value
-    const email=document.getElementById("email").value
-    const numCel = document.getElementById("num_celular").value
-    validation()
-    console.log("La persona ", name)
-    cleanForm()
 }
 
-function cleanForm(){
-    document.getElementById("name").value =""
-    document.getElementById("last_name").value ="" 
-    document.getElementById("num_Nights").value =""
-}
-/* 
-function llenarPersona(name, last_Name, num_Nights, email, numCel){
-
-    persona=[name, last_Name, num_Nights, email, numCel]
-    return persona[0]
-} */
-
-
-function validation(){
-    if (name == ""){
-        document.getElementById("name").focus()
-
-    }else if(last_Name == ""){
-        document.getElementById("last_name").focus()
-
-    }else if (num_Nights == 0){
-        document.getElementById("num_Nights").focus() 
-    }else{
-        cleanForm()
-    }
-    
-}
-
-const list_clients=document.querySelector("ul")
-const li=document.createElement('li')
-li.textContent= llenarPersona()
-list_clients.appendChild(li)
 
