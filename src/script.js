952574d3d4c463268e50a5d59ad9cc164e7d9864
German García S <german.gs19@gmail.com>
2022-11-16 20:51:15 -0600

    Function validatio and clearForm add

--- src/script.js
+++ src/script.js
@@ -1,13 +1,31 @@
 var getData = function(){
     persona=[]
-    for (var i = 0; i <=4; i++){
+   for (var i = 0; i <=4; i++){
         persona[i]=document.getElementById("persona"+(i+1)).value
     }
-
+    validation(persona)
     persona.forEach(element => {
         console.log(element)
         
     });
+
+    cleanForm(persona)
+}
+
+function cleanForm(persona){
+    for(var i=0; i<persona.length; i++){
+        document.getElementById("persona"+(i+1)).value = ""
+    }
+}
+
+function validation(persona){
+    
+    for (var i=0; i<persona.length; i++){
+        if(persona[i] ==""){
+            document.getElementById("persona"+(i+1)).focus()
+        }
+    }
+
 }
 
 
