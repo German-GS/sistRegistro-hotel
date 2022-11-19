eb429d08ccbf6690e88fd83ae5ffa8ea927d74f8
German García S <german.gs19@gmail.com>
2022-11-19 12:06:27 -0600

    modify bacground color and days.js

--- src/days.js
+++ src/days.js
@@ -2,7 +2,7 @@ function calcDays(persona){
     var dateCheckIn = new Date(persona[4])
     var dateChechOut = new Date(persona[5])
     var difDays = dateChechOut.getTime()- dateCheckIn.getTime()
-    return Math.round(difDays / (1000*60*60*24))
+    return Math.round((difDays - 1) / (1000*60*60*24))
 }
 
 export {calcDays}
